c93747b36707ba6d9e927f001e6bf3fc0276b49a
Robin Tram <robin.tram@gmail.com>
2018-10-25 13:59:32 +0000

    Done.

--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,22 @@
-# Enter your object-oriented solution here!
+require 'pry'
+
+class Multiples
+  
+  attr_accessor :limit
+  
+  def initialize(limit)
+    @limit = limit
+    raise ArgumentError if !(limit.is_a?Integer)
+  end 
+  
+  def collect_multiples
+    (1...limit).select do |el|
+      (el % 3 == 0 || el % 5 == 0)
+    end 
+  end
+
+
+  def sum_multiples
+    self.collect_multiples.inject(:+)
+  end 
+end 
