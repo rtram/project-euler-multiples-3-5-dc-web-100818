efbcd9ca8cff9090eb6ee471b7a903a28bd0006b
Robin Tram <robin.tram@gmail.com>
2018-10-25 13:55:00 +0000

    automatically backed up by learn

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,12 @@
-# Enter your procedural solution here!
+require 'pry'
+
+def collect_multiples(limit)
+  (1...limit).select do |el|
+    (el % 3 == 0 || el % 5 == 0)
+  end 
+end
+
+
+def sum_multiples(limit)
+  collect_multiples(limit).inject(:+)
+end 
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,21 @@
-# Enter your object-oriented solution here!
+require 'pry'
+
+class Multiples
+  
+  attr_accessor :limit
+  
+  def initialize(limit)
+    @limit = limit
+  end 
+  
+  def collect_multiples
+    (1...limit).select do |el|
+      (el % 3 == 0 || el % 5 == 0)
+    end 
+  end
+
+
+  def sum_multiples
+    self.collect_multiples.inject(:+)
+  end 
+end 
